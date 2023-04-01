2e1e1fa5eaafa13f3ba5972c2e1d9db014d0c495
Johnny <johnathan.mnn@gmail.com>
2023-03-31 21:33:39 -0500

    Started styling homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,15 @@
 import "../styles/styles.css";
+import "../styles/home.css";
+import bicep from "../assets/imgs/bicep.png";
 
 export default function Homepage(){
     return(
-        <div>
-            <h1>Fullstack React App</h1>
-            <h2>Powered by Johnny!</h2>
+        <div className="container" >
+            <div id="homepage">
+                <h1>FITNESS FINDER</h1>
+                <img src={bicep}/>
+                <h2>Start Now!</h2>
+            </div>
         </div>
     )
 }
